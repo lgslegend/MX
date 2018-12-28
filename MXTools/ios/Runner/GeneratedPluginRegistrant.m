397f4f1db7c07ae57a1fc53c515fd8d9bbda0383
Nightmare-MYS <44189674+Nightmare-MYS@users.noreply.github.com>
2018-12-28 21:27:53 +0800

    Add files via upload

---
+++ MXTools/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <package_info/PackageInfoPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+}
+
+@end
